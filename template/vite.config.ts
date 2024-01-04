b029f60b6b488ade1bb948c60a726ec23dfe5e57
Juan Pablo Solano <hello@wondercraft.co>
2024-01-04 14:31:10 -0500

    updates

--- template/vite.config.ts
+++ template/vite.config.ts
@@ -3,35 +3,30 @@ import fs from "fs";
 import path from "path";
 
 const inputDir = "./src/scripts"; // replace with your input directory
-const outputDir = "./hello"; // replace with your output directory
 
-const entries = fs
-  .readdirSync(path.resolve(__dirname, inputDir))
-  .filter((file) => file.endsWith(".js")) // only include .js files
-  .reduce((entries, file) => {
-    const name = path.basename(file, ".js"); // get the file name without the extension
-    // entries[path.join(outputDir, name)] = path.join(inputDir, file); // add the entry to the entries object
-    entries[path.join( name)] = path.join(inputDir, file); // add the entry to the entries object
-    return entries;
-  }, {});
-console.log("🚀 ~ file: vite.config.ts:15 ~ entries:", entries);
+const entries = (subDir = "") =>
+  fs
+    .readdirSync(path.resolve(__dirname, inputDir, subDir))
+    .filter((file) => file.endsWith(".js")) // only include .js files
+    .reduce((entries, file) => {
+      const name = path.basename(file, ".js"); // get the file name without the extension
+      entries[path.join(name)] = path.join(inputDir, subDir, file); // add the entry to the entries object
+      return entries;
+    }, {});
 
 export default {
   build: {
     outDir: "assets",
     rollupOptions: {
-      input: entries,
-      // output: {
-      //   assetFileNames: `${outputDir}/[name]`,
-      // },
+      input: { app: "src/app.js", ...entries("components") },
       output: {
         entryFileNames: `[name].js`,
         chunkFileNames: `[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
+        assetFileNames: `assets/[name].[ext]`,
+      },
     },
     rollupOutputOptions: {
-      entryFileNames: '[name]',
-    }
+      entryFileNames: "[name]",
+    },
   },
 };
