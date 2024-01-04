eca1db8af8d5386df85e52be6ec285c623292852
Juan Pablo Solano <hello@wondercraft.co>
2024-01-04 13:51:14 -0500

    build

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
 #!/usr/bin/env node
+import ora from "ora";
+import path from "path";
+import fs from "fs-extra";
+import { PKG_ROOT } from "~/consts.js";
+
 console.log("******************************");
 console.log("**********  index.js *********");
 console.log("******************************");
+
+const projectDir = process.argv[2];
+
+const init = async () => {
+  const projectDir = path.resolve(process.cwd());
+  const srcDir = path.join(PKG_ROOT, "template");
+  console.log("🚀 ~ file: index.ts:15 ~ init ~ srcDir:", srcDir);
+  console.log(projectDir);
+  const spinner = ora(`Scaffolding in: ${projectDir}...\n`).start();
+
+  fs.copySync(srcDir, projectDir);
+  process.exit(1);
+};
+
+init();
